3018ff2034c88e4d1117834161ba98f33473532e
Abhishek Jakkannavar <tracerabhi96@ymail.com>
2018-02-23 19:39:48 +0530

    With Banner Add On Home Screen,Feedback,About

--- app/src/main/java/shareholder/infinity/com/ladiesclub/AboutActivity.java
+++ app/src/main/java/shareholder/infinity/com/ladiesclub/AboutActivity.java
@@ -1,13 +1,27 @@
 package shareholder.infinity.com.ladiesclub;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 
 public class AboutActivity extends AppCompatActivity {
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+
+        //Advertisement Code
+        // Sample AdMob app ID: ca-app-pub-3940256099942544~3347511713
+        MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713");
+
+        mAdView = findViewById(R.id.adViewaboutsection);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
     }
 }
--- app/src/main/java/shareholder/infinity/com/ladiesclub/FeedbackActivity.java
+++ app/src/main/java/shareholder/infinity/com/ladiesclub/FeedbackActivity.java
@@ -1,16 +1,21 @@
 package shareholder.infinity.com.ladiesclub;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
 public class FeedbackActivity extends AppCompatActivity {
 
     WebView wv;
     String myPage ="https://docs.google.com/forms/d/e/1FAIpQLSd3kSklSZrhzPQcSxQsnDD14OQXZDMMo83j2amvJ7w2KzK5-Q/viewform";
+    private AdView mAdView;
 
 
     @Override
@@ -18,7 +23,7 @@ public class FeedbackActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
 
-        wv =(WebView)findViewById(R.id.feedbackid);
+        wv = findViewById(R.id.feedbackid);
         wv.getSettings().getJavaScriptEnabled();
         wv.getSettings().setJavaScriptEnabled(true);
         wv.setFocusable(true);
@@ -31,5 +36,14 @@ public class FeedbackActivity extends AppCompatActivity {
         wv.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
         wv.setWebViewClient(new WebViewClient());//this line opens the link in the same browser
         wv.loadUrl(myPage);
+
+
+        //Advertisement Code
+        // Sample AdMob app ID: ca-app-pub-3940256099942544~3347511713
+        MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713");
+
+        mAdView = findViewById(R.id.adViewfeedbacksection);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
     }
 }
